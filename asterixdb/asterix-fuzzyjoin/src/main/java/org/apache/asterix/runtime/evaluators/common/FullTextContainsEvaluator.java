b4286254ba098166ff59ae98c2f3a959a53d22a3
Rui Guo <ruig2@uci.edu>
2020-04-23 10:38:17 -0700

    polish

--- asterixdb/asterix-fuzzyjoin/src/main/java/org/apache/asterix/runtime/evaluators/common/FullTextContainsEvaluator.java
+++ asterixdb/asterix-fuzzyjoin/src/main/java/org/apache/asterix/runtime/evaluators/common/FullTextContainsEvaluator.java
@@ -62,9 +62,6 @@ import org.apache.logging.log4j.Logger;
 public class FullTextContainsEvaluator implements IScalarEvaluator {
     private static final Logger LOGGER = LogManager.getLogger();
 
-    // assuming type indicator in serde format
-    protected static final int TYPE_INDICATOR_SIZE = 1;
-
     protected final ArrayBackedValueStorage resultStorage = new ArrayBackedValueStorage();
     protected final DataOutput out = resultStorage.getDataOutput();
     protected final TaggedValuePointable argLeft = TaggedValuePointable.FACTORY.createPointable();
@@ -338,8 +335,8 @@ public class FullTextContainsEvaluator implements IScalarEvaluator {
             }
             keyEntry.set(tokenOffset, tokenLength);
 
-            String rightTokenStr = getUTF8StringInArray(token.getData(), tokenOffset, tokenLength);
-            //LOGGER.info("Right token: " + rightTokenStr);
+            // String rightTokenStr = getUTF8StringInArray(token.getData(), tokenOffset, tokenLength);
+            // LOGGER.info("Right token: " + rightTokenStr);
 
             // Check whether the given token is a phrase.
             // Currently, for the full-text search, we don't support a phrase search yet.
@@ -358,8 +355,6 @@ public class FullTextContainsEvaluator implements IScalarEvaluator {
                 rightHashSet.put(keyEntry);
                 uniqueQueryTokenCount++;
             }
-
-            //LOGGER.info("uniqueQueryTokenCount: " + uniqueQueryTokenCount);
         }
 
         // Based on the search mode option - "any" or "all", set the occurrence threshold of tokens.
@@ -429,27 +424,14 @@ public class FullTextContainsEvaluator implements IScalarEvaluator {
         // The left side: field (document)
         // Resets the tokenizer for the given keywords in a document.
 
-        int startOffset = arg1.getStartOffset();
-        int length = arg1.getLength();
-        /*
-        // How many bytes are required to store the length of the given string?
-        int numBytesToStoreLength = UTF8StringUtil
-                .getNumBytesToStoreLength(UTF8StringUtil.getUTFLength(arg1.getByteArray(), arg1.getStartOffset()));
-        int startOffset +=  numBytesToStoreLength;
-        int length -=  numBytesToStoreLength;
-         */
-
-        // LOGGER.info("entire sentence: " + getUTF8StringInArray(arg1.getByteArray(), startOffset + 2, length - 2));
-
-        configLeft.reset(arg1.getByteArray(), startOffset, length);
+        configLeft.reset(arg1.getByteArray(), arg1.getStartOffset(), arg1.getLength());
 
         // Creates tokens from a field in the left side (document)
         while (configLeft.hasNext()) {
             configLeft.next();
 
             IToken token = configLeft.getToken();
-
-            String leftTokenStr = getUTF8StringInArray(token.getData(), token.getStartOffset(), token.getTokenLength());
+            // String leftTokenStr = getUTF8StringInArray(token.getData(), token.getStartOffset(), token.getTokenLength());
 
             // Records the starting position and the length of the current token.
             keyEntry.set(token.getStartOffset(), token.getTokenLength());
@@ -458,9 +440,7 @@ public class FullTextContainsEvaluator implements IScalarEvaluator {
             // We don't count multiple occurrence of a token now.
             // So, finding the same query predicate twice will not be counted as a found.
             if (rightHashSet.find(keyEntry, token.getData(), true) == 1) {
-                //LOGGER.info("\t\t\tHit!!!");
                 foundCount++;
-                //LOGGER.info("\t\t" + foundCount);
                 if (foundCount >= occurrenceThreshold) {
                     return true;
                 }
