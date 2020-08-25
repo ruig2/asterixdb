84a8d063371aae37478cb9450bb0a2cab4d53b12
Rui Guo <ruig2@uci.edu>
2020-08-25 09:32:03 -0700

    added py script

--- asterixdb/asterix-app/src/test/resources/runtimets/queries_sqlpp/dml/create_string_test_case.py
+++ asterixdb/asterix-app/src/test/resources/runtimets/queries_sqlpp/dml/create_string_test_case.py
@@ -6,10 +6,10 @@ import shutil
 
 STRING_AS_PRIMARY_KEY = "-string-as-primary-key"
 
-folder_name_old = sys.argv[1]
+folder_name_old = sys.argv[1][:-22]
 folder_name_new = folder_name_old + STRING_AS_PRIMARY_KEY
 
-# os.mkdir(folder_name_new)
+os.mkdir(folder_name_new)
 
 print folder_name_old
 print folder_name_new
@@ -19,7 +19,7 @@ for file_name_old in os.listdir(folder_name_old):
     file_name_new = splits[0] + STRING_AS_PRIMARY_KEY
     for name in splits[1:]:
         file_name_new += "." + name
-    # shutil.copy2(folder_name_old + "/" + file_name_old, folder_name_new + "/" + file_name_new)
+    shutil.copy2(folder_name_old + "/" + file_name_old, folder_name_new + "/" + file_name_new)
 
 old_path = os.getcwd()
 new_path = old_path.replace("queries_sqlpp", "results", 1)
